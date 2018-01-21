9314f2d7960cbdd55616bff21e9a590a81c03a45
Miguel Mendes <1120588@isep.ipp.pt>
2018-01-21 22:58:47 +0000

    removed codcov.

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -19,22 +19,6 @@ main() {
 
     cross rustc --bin img_diff --target $TARGET --release -- -C lto
 
-# codecov triggers
-    wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz
-    tar xzf master.tar.gz
-    cd kcov-master
-    mkdir build
-    cd build
-    cmake ..
-    make
-    make install DESTDIR=../../kcov-build
-    cd ../..
-    rm -rf kcov-master
-    ls -aR target/$TARGET/release
-    for file in target/$TARGET/release/img_diff*[^\.d]; do mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done
-    curl -s "https://codecov.io/bash"
-    echo "Uploaded code coverage"
-
     cp target/$TARGET/release/img_diff $stage/
 
     cd $stage
