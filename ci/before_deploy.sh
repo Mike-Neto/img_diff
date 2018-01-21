423fba81fae39d76757c9847748e60315fb75537
Miguel Mendes <1120588@isep.ipp.pt>
2018-01-21 01:44:47 +0000

    fix ci configs

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,9 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     cross rustc --bin img_diff --target $TARGET --release -- -C lto
-
+# DEBUG
+    ls target/$TARGET/release/
+# DEBUG
     cp target/$TARGET/release/img_diff $stage/
 
     cd $stage
