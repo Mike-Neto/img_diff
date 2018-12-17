7a2316e8db69667a4b70bb5cc42f3e014752a29c
Miguel Mendes <miguelmendeseiriz@gmail.com>
2018-12-17 13:29:09 +0000

    Updated CI removed fmt clippy & bench

--- ci/script.sh
+++ ci/script.sh
@@ -13,11 +13,9 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross bench --target $TARGET --no-run
-
-    cross fmt
-    
-    cross clippy
+    #cross bench --target $TARGET --no-run
+    #cross fmt
+    #cross clippy
 }
 
 # we don't run the "test phase" when doing deploys
